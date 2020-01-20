4b92baaace8036386171935c0a645cf254fe2dbd
Miguel Sepulcre <msepulcre@umh.es>
2020-01-20 16:24:03 +0100

    Update copyright

--- scratch/dcc-example.cc
+++ scratch/dcc-example.cc
@@ -1,5 +1,5 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
+ * Copyright (c) 2020, Universidad Miguel Hernandez de Elche (UMH)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -14,9 +14,10 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Authors: Jorge Mira <jorge.mira01@alu.umh.es>
- * 			Gokulnath Thandavarayan <gthandavarayan@umh.es>
- * 
+ * Authors: Jorge Mira <jorge.mira01@alu.umh.es> (main contributor)
+ * 	    Gokulnath Thandavarayan <gthandavarayan@umh.es>
+ * 	    Miguel Sepulcre <msepulcre@umh.es>
+ *          Javier Gozalvez <j.gozalvez@umh.es>
  *
  */
 

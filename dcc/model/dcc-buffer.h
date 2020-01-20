45869506da1de2051195afa05faa589b7701d39c
Miguel Sepulcre <msepulcre@umh.es>
2020-01-20 17:58:21 +0100

    Update copyright

--- dcc/model/dcc-buffer.h
+++ dcc/model/dcc-buffer.h
@@ -1,9 +1,26 @@
-/* */
- /* dcc-buffer.h
+ /*
+ * Copyright (c) 2020, Universidad Miguel Hernandez de Elche (UMH)
  *
- *  Created on: 23 mar. 2019
- *      Author: Jorge Mira <jorge.mira01@alu.umh.es>
- */
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Jorge Mira <jorge.mira01@alu.umh.es> (main contributor)
+ * 	    Gokulnath Thandavarayan <gthandavarayan@umh.es>
+ * 	    Miguel Sepulcre <msepulcre@umh.es>
+ *          Javier Gozalvez <j.gozalvez@umh.es>
+ *          UWICORE laboratory, http://www.uwicore.umh.es
+ *          Universidad Miguel Hernandez de Elche (UMH), Spain
+*/
 
 
 #ifndef SRC_DCC_MODEL_DCC_BUFFER_H_
